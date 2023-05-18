6150d1dc7ba7110b6a1828f2356ac9b3dc90a406
Modjoul-William <william.rose@modjoul.com>
2023-05-18 16:45:52 -0400

    Add additional methods

--- mfa_creds.py
+++ mfa_creds.py
@@ -6,69 +6,108 @@ load_dotenv()
 
 class MFACreds:
 
+    """
+    Class to handle AWS authentication and generate temporary credentials from MFA
+    """
+
     def __init__(self):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         self.file_path = os.path.join(current_dir, 'temp.env')
-        print(self.file_path)
         self.access_key = None
         self.secret_access_key = None
         self.session_token = None
 
-    def obtain_MFA_credentials(self):
+    def obtain_temp_MFA_credentials(self):
         """
         Takes MFA code and obtains temporary credentials required to use boto3
         """
 
-        TokenCode = input('What is your MFA code for getintoawsmodjoul?')
+        mfa_code = input('What is your MFA code for getintoawsmodjoul?')
 
         try:
             sts_client = boto3.client('sts')
+            # Identifier can be found in your security credentials in AWS account
             res = sts_client.get_session_token(SerialNumber = os.environ['IDENTIFIER'],
-                                            TokenCode = TokenCode)
+                                            TokenCode = mfa_code)
         except Exception as e:
             print("\nThere was an error obtaining credentials.\n")
-            print("Please try waiting for your MFA code to refresh.\n")
+            print(e)
             return None
         
-        access_key = res['Credentials']['AccessKeyId']
-        secret_access_key = res['Credentials']['SecretAccessKey']
-        session_token = res['Credentials']['SessionToken']
-
-        print(f"\nAccess Key: {access_key}\n")
-        print(f"secret_access_key: {secret_access_key}\n")
-        print(f"session_token: {session_token}\n")
-
-        self.access_key = access_key
-        self.secret_access_key = secret_access_key
-        self.session_token = session_token
+        self.access_key = res['Credentials']['AccessKeyId']
+        self.secret_access_key = res['Credentials']['SecretAccessKey']
+        self.session_token = res['Credentials']['SessionToken']
 
         return True
 
+    def create_AWS_connection(self, service, connection_type):
+        """
+        Create AWS client or resource using temp credentials
+
+        Parameters:
+        service (str): The name of the AWS service to connect. This should be 
+                    a string like 's3', 'ec2', etc.
+        connection_type (str): The type of connection. This should be either 'client' or 'resource'.
+        """
+
+        if connection_type == 'client':
+            connection = boto3.client(
+                service,
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret_access_key,
+                aws_session_token=self.session_token,
+                region_name=os.getenv('AWS_DEFAULT_REGION')
+            )
+        elif connection_type == 'resource':
+            connection = boto3.resource(
+                service,
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret_access_key,
+                aws_session_token=self.session_token,
+                region_name=os.getenv('AWS_DEFAULT_REGION')
+            )
+        else:
+            raise ValueError(f"Invalid connection_type {connection_type}. Expected 'client' or 'resource'.")
+
+        return connection
+
     def write_creds_to_file(self):
         """
-        Writes temporary credentials obtained in obtain_MFA_credentials to current file path
+        Writes temporary credentials obtained in obtain_temp_MFA_credentials to current file path
         """
-        lines = [
-            f"""AWS_ACCESS_KEY = '{self.access_key}'""",
-            f"""AWS_SECRET_ACCESS_CODE = '{self.secret_access_key}'""",
-            f"""AWS_SESSION_TOKEN = '{self.session_token}'""",
-            """AWS_REGION = 'us-east-1'"""
-            ]
-        try:
-            with open(self.file_path, 'w') as f:
-                for line in lines:
-                        f.write(line)
-                        f.write('\n')
-        except Exception as e:
-            print(f'There was an issue {e}')
+        if self.access_key is not None:
+            lines = [
+                f"""AWS_ACCESS_KEY = '{self.access_key}'""",
+                f"""AWS_SECRET_ACCESS_CODE = '{self.secret_access_key}'""",
+                f"""AWS_SESSION_TOKEN = '{self.session_token}'""",
+                """AWS_REGION = 'us-east-1'"""
+                ]
+            try:
+                with open(self.file_path, 'w') as f:
+                    for line in lines:
+                            f.write(line)
+                            f.write('\n')
+            except Exception as e:
+                print(f'There was an issue {e}')
+        else:
+            return None
+        
+    def hello_world_test(self):
+        """
+        Method to verify that connection to aws services is working properly
+        """
+        self.obtain_temp_MFA_credentials()
+        client = self.create_AWS_connection('s3', 'client')
+        for resp in client.list_objects(Bucket=os.getenv('AWS_BUCKET_NAME'), Prefix="2023/05")["Contents"]:
+            file_name = client.get_object(Bucket=os.getenv('AWS_BUCKET_NAME'), Key=resp['Key'])
+            print(file_name)
+            break # this will exit the loop after one iteration
+
 
 def main():
 
     credentials = MFACreds()
-
-    if credentials.obtain_MFA_credentials() is not None:
-        credentials.write_creds_to_file()
-
-
+    credentials.hello_world_test()
+    
 if __name__ == '__main__':
     main()
